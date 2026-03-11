e0a93fefcbd9db881a93a9f5c641779633ea3ca4
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2026-03-11 17:32:08 +0100

    fix(type): lax on list as Sequence

--- src/toml_decouple/toml_types.py
+++ src/toml_decouple/toml_types.py
@@ -1,9 +1,13 @@
-from typing import Literal
+from collections.abc import Sequence
 from datetime import date, datetime, time
+from typing import Literal
 
 type TomlValue = (
-    bool | date | datetime | time | TomlDict | float | int | list[TomlValue] | str
+    bool | date | datetime | float | int | str | time | Sequence[TomlValue] | TomlDict
 )
+
 type OptTomlValue = TomlValue | None
+
 type TomlDict = dict[str, OptTomlValue]
+
 type NoDefault = tuple[Literal["NO"], Literal["DEFAULT"]]  # optparse.NO_DEFAULT
